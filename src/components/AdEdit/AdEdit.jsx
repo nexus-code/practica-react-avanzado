211a453edcbbf7d8f20ceb57ca49670b04e328ad
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2019-12-14 12:55:31 +0100

    Improve getAd: only from store (via LocalStore)

--- src/components/AdEdit/AdEdit.jsx
+++ src/components/AdEdit/AdEdit.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams, useHistory } from 'react-router';
-import { useGetAdFromAPI, getAdFromStore } from '../../store/ads/selectors';
+import { getAd } from '../../store/ads/selectors';
 import HookForm from '../Form/HookForm'
 import AppNavbar from '../AppNavbar/AppNavbar';
 import { Button } from 'react-bootstrap';
@@ -12,21 +12,8 @@ function AdEdit(props) {
     const { id } = useParams();
     const history = useHistory();
     const { ads } = props;
-
-    // if (typeof ads.length === 'undefined' ){
-    //     return<>
-    //         <AppNavbar />
-    //         <div className="container">
-    //             <br />
-    //             <h3> Incorrect access </h3>
-    //             <p>Follow the usual path to edit an ad </p>
-    //             <br />
-    //             <Button className='btn btn-default' onClick={() => history.push(`/`)} >Go Home</Button>
-    //         </div>;
-    //     </>
-    // }
-   
-    const ad = useGetAdFromAPI(ads, id);
+ 
+    const ad = getAd(props, id);
     // console.log('AdEdit ad ', ad);
 
     const formProps = {
--- src/store/ads/selectors.js
+++ src/store/ads/selectors.js
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import { getAdDetail } from '../../services/AdService';
-import { fetchAds } from '../../store/ads/actions';
-
 export function getAds(ads, filter) {
 
     // basic implementation
@@ -9,74 +5,19 @@ export function getAds(ads, filter) {
     return ads;
 }
 
-
-export const useGetAdFromAPI = (id) => {
-    // Search the ad in API 
-
-    const [response, setResponse] = useState(null);
-    const [error, setError] = useState(null);
-
-
-    getAdDetail(id).then(ad => {
-
-        if (ad.hasOwnProperty('success')) {
-
-            setError('404');
-        } else {
-
-            setResponse(ad);
-        }
-    });
-
-    return error === '404' ? null : response;
-};
-
-
-// DEPRECATED!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-export const getAdFromStore = (ads, id) => {
-
-
-    // console.log('getAdFromStore', ads);
-    // console.log('getAdFromStore []', ads[ads]);
-    // console.log('getAdFromStore ads.length', ads.length);
-
-
-    //if (typeof ads === 'undefined' || typeof id === 'undefined') {
-    if (ads.length === 'undefined' || typeof id === 'undefined'){
-        return null
-    }
-
-    // ad stored by redux
-    return ads.filter(ad => ad.id === id)[0];
-};
-/////////////////////////////////////////
-
-
 export const getAd = (props, id) => {
 
-    //props.ads.ads array of ads in redux
-
-    const { ads } = props.ads;
-
-    return ads.filter(ad => ad.id === id)[0];
-};
-
-export const useGetAd = (ads, id) => {
-
-    if (!id)
-        return null; // improve this if (control input)
-
-
-    if (typeof ads.length === 'undefined'){
-        // WARNING: hook in conditional statement: fetch add from store or API.
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        return useGetAdFromAPI(id)
+    //Improve this on store!!
+    
+    try{
+        
+        return props.ads.filter(ad => ad.id === id)[0];
+    }
+    catch(err){
+        
+        return props.ads.ads.filter(ad => ad.id === id)[0];
     }
 
-    // Improve this:
-    // WARNING: hook in conditional statement: fetch add from store or API. 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    return typeof ads.length !== 'undefined' ? getAdFromStore(ads, id) : useGetAdFromAPI(id);
 };
 
 export const editAd = (id) => {
