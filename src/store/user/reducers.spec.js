83730b0e0191cd050a06f83046eb524b566cf61c
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2020-01-15 12:23:43 +0100

    adding testing for reducers

--- src/store/user/reducers.spec.js
+++ src/store/user/reducers.spec.js
@@ -3,19 +3,31 @@ import * as TYPES from './types';
 
 describe('user reducers', () => {
   
-  const initialState = {name: 'user name', surname: 'user_surname'};
+  const initialState = { 
+    user: { name: 'initial user name', surname: 'initial user surname' },
+    ads: [],
+    lastAction: {},
+  };
 
-  it('dummy_action: should return the initial state', () => {
+  it('send dummy_action: should return the initial state', () => {
     const action = { type: 'dummy_action' };
     expect(user(initialState, action)).toEqual(initialState);
   })
 
-  it('SET_USER: should return empty string', () => {
-    const action = { type: TYPES.SET_USER };
-    expect(user(initialState, action)).toEqual(initialState);
+  it('send TYPES.SET_USER action: should return empty string', () => {
+    const testUSer = { name: 'user_name', surname: 'user_surname'};
+    const action = { type: TYPES.SET_USER, user: testUSer };
+    const expectedState = {
+      user: testUSer,
+      ads: [],
+      lastAction: {},
+    };
+
+    console.log('action', action);
+    expect(user(initialState, action)).toEqual(expectedState);
   })
 
-  it('LOGOUT: should return empty string', () => {
+  it('send TYPES.LOGOUT action: should return empty string', () => {
     const action = { type: TYPES.LOGOUT };
     expect(user(initialState, action)).toEqual('');
   })
